5c6c03cdd99c70253b3491b92af2eda1f24e31d6
DUPRE Raphaël <dupre@ipno.in2p3.fr>
2023-03-17 17:38:11 +0100

    Update qweight.f

--- qweight.f
+++ qweight.f
@@ -46,7 +46,7 @@ c       Determine calculated transverse momentum of final parton
             else if(iPtF.eq.1) then
               ipt = qhat*QW_L
             else if(iPtF.eq.2) then
-              ipt = 8*QW_w/3/alphas/QW_L*SupFac**2
+              ipt = 4*QW_w/3/alphas/QW_L*SupFac**2
             else if(iPtF.eq.3) then
               ipt = (QW_w*sin(QW_th)*SupFac)**2
             endif
